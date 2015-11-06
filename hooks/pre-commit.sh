06e4018b2159eabd8c63273d05bb75072d697c3f
Dirk Fuchs <difu@mac.com>
2015-11-06 11:17:48 +0100

    Variable usage

--- hooks/pre-commit.sh
+++ hooks/pre-commit.sh
@@ -2,5 +2,5 @@
 # Ensure that all unit tests are successful
 python3 -m unittest discover tests/
 RESULT_UNITTEST=$?
-[ $RESULT_UNITTEST -ne 0 ] && exit 1
+[ ${RESULT_UNITTEST} -ne 0 ] && exit 1
 exit 0
