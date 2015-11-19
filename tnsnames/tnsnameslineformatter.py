5c96e536a9f3603b0bae0b72c69f3daaec322f66
Dirk Fuchs <difu@mac.com>
2015-11-19 19:04:23 +0100

    Bugfix: Call methods of superclass

--- tnsnames/tnsnameslineformatter.py
+++ tnsnames/tnsnameslineformatter.py
@@ -8,8 +8,10 @@ class TnsnameLineFormatter(TnsnamesFormatter):
 
     # Enter a parse tree produced by tnsnamesParser#tns_entry.
     def enterTns_entry(self, ctx: tnsnamesParser.Tns_entryContext):
+        super().enterTns_entry(ctx)
         self._lines.append(ctx.getText())
 
     # Enter a parse tree produced by tnsnamesParser#lsnr_entry.
     def enterLsnr_entry(self, ctx: tnsnamesParser.Lsnr_entryContext):
+        super().enterLsnr_entry(ctx)
         self._lines.append(ctx.getText())
--- tnsnames/tnsnamesorastyleformatter.py
+++ tnsnames/tnsnamesorastyleformatter.py
@@ -50,31 +50,36 @@ class TnsnameOraStyleFormatter(TnsnamesFormatter):
 
     # Exit a parse tree produced by tnsnamesParser#description.
     def exitDescription(self, ctx: tnsnamesParser.DescriptionContext):
+        super().exitDescription(ctx)
         self._level -= 1
         line_string = self.get_indents_string() + ")"
         self._lines.append(line_string)
 
     # Enter a parse tree produced by tnsnamesParser#d_sdu.
     def enterD_sdu(self, ctx: tnsnamesParser.D_sduContext):
+        super().enterD_sdu(self)
         self._current_line = ctx.getText()
         self.append_current_line()
 
     # Enter a parse tree produced by tnsnamesParser#d_conn_timeout.
     def enterD_conn_timeout(self, ctx: tnsnamesParser.D_conn_timeoutContext):
+        super().enterD_conn_timeout(ctx)
         self._current_line = ctx.getText()
         self.append_current_line()
 
     # Exit a parse tree produced by tnsnamesParser#d_conn_timeout.
     def exitD_conn_timeout(self, ctx: tnsnamesParser.D_conn_timeoutContext):
-        pass
+        super().exitD_conn_timeout(ctx)
 
     # Enter a parse tree produced by tnsnamesParser#d_retry_count.
     def enterD_retry_count(self, ctx: tnsnamesParser.D_retry_countContext):
+        super().exitD_retry_count(ctx)
         self._current_line = ctx.getText()
         self.append_current_line()
 
     # Enter a parse tree produced by tnsnamesParser#address_list.
     def enterAddress_list(self, ctx: tnsnamesParser.Address_listContext):
+        super().enterAddress_list(ctx)
         line_string = self.get_indents_string() + "(" + tnsnamesParser.ruleNames[
             tnsnamesParser.RULE_address_list] + "="
         self._level += 1
@@ -82,34 +87,37 @@ class TnsnameOraStyleFormatter(TnsnamesFormatter):
 
     # Exit a parse tree produced by tnsnamesParser#address_list.
     def exitAddress_list(self, ctx: tnsnamesParser.Address_listContext):
+        super().exitAddress_list(ctx)
         self._level -= 1
         line_string = self.get_indents_string() + ")"
         self._lines.append(line_string)
 
     # Enter a parse tree produced by tnsnamesParser#al_load_balance.
     def enterAl_load_balance(self, ctx: tnsnamesParser.Al_load_balanceContext):
+        super().enterAl_load_balance(ctx)
         self._current_line = ctx.getText()
         self.append_current_line()
 
     # Exit a parse tree produced by tnsnamesParser#al_load_balance.
     def exitAl_load_balance(self, ctx: tnsnamesParser.Al_load_balanceContext):
-        pass
+        super().exitAl_load_balance(ctx)
 
     # Enter a parse tree produced by tnsnamesParser#address.
     def enterAddress(self, ctx: tnsnamesParser.AddressContext):
+        super().enterAddress(ctx)
         ret_string = self.get_indents_string() + ctx.getText()
         self._lines.append(ret_string)
 
     # Exit a parse tree produced by tnsnamesParser#address.
     def exitAddress(self, ctx: tnsnamesParser.AddressContext):
-        pass
+        super().exitAddress(ctx)
 
     # Enter a parse tree produced by tnsnamesParser#host.
     def enterHost(self, ctx: tnsnamesParser.HostContext):
         # ret_string = self.get_indents_string() + ctx.getRuleContext().parentCtx.getText()
         # self._lines.append(ret_string)
-        pass
+        super().enterHost(ctx)
 
     # Exit a parse tree produced by tnsnamesParser#host.
     def exitHost(self, ctx: tnsnamesParser.HostContext):
-        pass
+        super().exitHost(ctx)
