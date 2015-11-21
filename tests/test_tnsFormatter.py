96fcf322afd4ce0a2a5da368cf1e9d411a770f48
Dirk Fuchs <difu@mac.com>
2015-11-21 15:25:08 +0100

    Added unittest: Test, if false tnsnames.ora is recognised.

--- tests/test_tnsFormatter.py
+++ tests/test_tnsFormatter.py
@@ -2,6 +2,7 @@ import os
 from unittest import TestCase
 
 from antlr4 import FileStream, CommonTokenStream, ParseTreeWalker
+from antlr4.error.ErrorListener import ErrorListener
 
 from tnsnames.tnsnamesLexer import tnsnamesLexer
 from tnsnames.tnsnamesParser import tnsnamesParser
@@ -10,12 +11,30 @@ from tnsnames.tnsnameslineformatter import TnsnameLineFormatter
 __author__ = 'dirkfuchs'
 
 
+class TnsErrorListenerException(ErrorListener):
+    def syntaxError(self, recognizer, offendingSymbol, line, column, msg, e):
+        raise Exception("Syntax error:  line " + str(line) + ":" + str(column) + " " + msg)
+
 class TestTnsFormatter(TestCase):
     _tnsnames_file = None
+    _script_path = None
 
     def setUp(self):
-        path = os.path.dirname(os.path.abspath(__file__))
-        self._tnsnames_file = '{0}/testFiles/tnsnames.ora'.format(path)
+        self._script_path = os.path.dirname(os.path.abspath(__file__))
+        self._tnsnames_file = '{0}/testFiles/tnsnames.ora'.format(self._script_path)
+
+    def test_bad_tnsnames_file(self):
+        tnsnames_file = '{0}/testFiles/tnsnames_false.ora'.format(self._script_path)
+        input_file_stream = FileStream(tnsnames_file)
+        errorListener = TnsErrorListenerException()
+
+        lexer = tnsnamesLexer(input_file_stream)
+        stream = CommonTokenStream(lexer)
+        parser = tnsnamesParser(stream)
+        parser._listeners.clear()
+        parser.addErrorListener(errorListener)
+        with self.assertRaisesRegexp(Exception, "Syntax error") as cm:
+            tree = parser.tnsnames()
 
     def test_format_single_line(self):
         input_file_stream = FileStream(self._tnsnames_file)
